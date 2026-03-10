5e6a4dc305eb5d3dfe0c0859682de03edf2a1db5
ampilon <ampilon@ucdavis.edu>
2026-03-10 10:24:08 -0700

    fish

---
+++ Shiny App/shiny_app.R
@@ -0,0 +1,62 @@
+
+library(tidyverse)
+library(janitor)
+library(naniar)
+library(ggmap)
+library(lubridate)
+library(shiny)
+library(dplyr)
+library(shinydashboard)
+library(ggplot2)
+
+microplastics <- read.csv("microplastics.csv")
+
+ui <- dashboardPage(
+  
+  skin = "purple",
+  
+  dashboardHeader(title = "Fish Microplastics"),
+  
+  dashboardSidebar(
+    
+    selectInput("z",
+                "Select a variable of interest",
+                choices = c("color", "particle_type", "sample_type", "species"),
+                selected = "color"),
+    
+    selectInput("x",
+                "Select a variable of interest",
+                choices = c("station", "species"),
+                selected = "station")
+  ),
+  
+  dashboardBody(
+    
+    plotOutput("plot", width = "500px", height = "400px")
+    
+  )
+)
+
+server <- function(input, output, session) {
+  
+  output$plot <- renderPlot({
+    
+    microplastics %>%
+      ggplot(aes(x = .data[[input$x]], fill = .data[[input$z]])) +
+      geom_bar(position = "dodge") +
+      theme_classic() +
+      labs(x = input$x, fill = input$z)
+    
+  })
+}
+
+shinyApp(ui, server)
+```
+
+bar plot for count by station
+```{r}
+microplastics %>%
+  ggplot(aes(x= station, fill = color)) +
+  geom_bar(position = "dodge") +
+  theme_classic() +
+  labs(x = "Station", y = "Count of Fish Collected", title = "Count of Fish Collected by Station")
